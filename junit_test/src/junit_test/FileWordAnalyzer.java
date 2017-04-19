a49fe5c3303ac78299624eeb0fed786d7453b5c9
redgyuf <faramirex070@gmail.com>
2017-04-19 14:01:32 +0200

    analyzer implemented

--- junit_test/src/junit_test/FileWordAnalyzer.java
+++ junit_test/src/junit_test/FileWordAnalyzer.java
@@ -1,5 +1,35 @@
 package junit_test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class FileWordAnalyzer {
+	FilePartReader fpr = null;
+
+	public FileWordAnalyzer(FilePartReader fpr) {
+		super();
+		this.fpr = fpr;
+	}
+
+	public ArrayList wordsByABC() throws IOException {
+		String input = fpr.readLines();
+		ArrayList<String> orderedLines = new ArrayList();
+		for (String word : input.split(" ")) {
+			orderedLines.add(word);
+		}
+		orderedLines.sort(String::compareToIgnoreCase);
+		return orderedLines;
+	}
+
+	public ArrayList wordsContainingSubString(String subString) throws IOException {
+		String input = fpr.readLines();
+		ArrayList<String> containedWords = new ArrayList();
+		for (String word : input.split(" ")) {
+			if (word.toLowerCase().contains(subString)) {
+				containedWords.add(word);
+			}
+		}
+		return containedWords;
+	}
 
 }
